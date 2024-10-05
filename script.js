36feabe116c76d4b8e041a7655c9e561fdc6dbf4
Aaron <trapp.root@gmail.com>
2024-10-04 19:16:15 -0700

    App fully functional, need styling tweaks

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 import { DUMMY_DATA } from './dummydata.js';
 
-// const API_KEY = '6A5BZCC93UM99R3GT2ARTW6QK';
+const API_KEY = '6A5BZCC93UM99R3GT2ARTW6QK';
+let city;
 
-async function getWeatherData(city = 'Portland') {
-	// const response =
-	// 	await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next7days?unitGroup=us&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cconditions%2Cdescription%2Cicon&include=fcst%2Chours%2Ccurrent%2Cdays&key=${API_KEY}&options=nonulls&contentType=json
-	// `);
-	// const weatherData = await response.json();
-	const weatherData = await new Promise((resolve) => {
-		setTimeout(() => resolve(DUMMY_DATA), 0.5);
-	});
+async function getWeatherData() {
+	const searchInput = document.querySelector('.search-input');
+	if (searchInput.value) {
+		city = searchInput.value;
+	} else {
+		city = 'Portland';
+	}
+	const response =
+		await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/next7days?unitGroup=us&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cconditions%2Cdescription%2Cicon&include=fcst%2Chours%2Ccurrent%2Cdays&key=${API_KEY}&options=nonulls&contentType=json
+	`);
+	const weatherData = await response.json();
 
 	hideLoadingState();
 	formatWeatherData(weatherData);
@@ -26,17 +30,23 @@ function formatWeatherData(data) {
 			temp: day.temp,
 		};
 	});
+	renderLocation(currentConditions.location);
 	renderCurrentConditions(currentConditions);
 	renderFutureConditions(futureConditions);
 }
 
+function renderLocation(location) {
+	const locationSpan = document.querySelector('.current-location');
+	locationSpan.innerText = '';
+	locationSpan.innerText = location;
+}
+
 function renderCurrentConditions(currentConditions) {
-	const { conditions, location, icon, temp } = currentConditions;
+	const { conditions, icon, temp } = currentConditions;
 	const currentWeatherContainer = document.querySelector('.current-weather');
 
 	const iconImg = createElement('img', 'current-icon', `./icons/${icon}.svg`);
 	const tempHeading = createElement('h1', 'current-temp', '', `${temp}°`);
-	const locationHeading = createElement('h2', 'current-location', '', location);
 	const conditionHeading = createElement(
 		'h1',
 		'current-condition',
@@ -44,12 +54,7 @@ function renderCurrentConditions(currentConditions) {
 		conditions
 	);
 
-	currentWeatherContainer.append(
-		iconImg,
-		tempHeading,
-		locationHeading,
-		conditionHeading
-	);
+	currentWeatherContainer.append(iconImg, tempHeading, conditionHeading);
 }
 
 function renderFutureConditions(futureConditions) {
@@ -76,15 +81,14 @@ function createElement(type, classname, src = '', innerText = '') {
 }
 
 function hideLoadingState() {
-	const searchAndLocationContainer = document.querySelector(
-		'.search-and-location'
-	);
 	const currentWeatherContainer = document.querySelector('.current-weather');
 	const weatherForecastContainer = document.querySelector('.weather-forecast');
 
-	searchAndLocationContainer.innerHTML = '';
 	currentWeatherContainer.innerHTML = '';
 	weatherForecastContainer.innerHTML = '';
 }
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', getWeatherData);
 
 getWeatherData();
